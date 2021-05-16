721d0fddb57378e3285f524900819f2866396b93
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 17:26:02 +0530

    Added Accounts & Customer Queries Contract Implementation

---
+++ src/Accounting.Infrastructure/Persistance/MongoDataAccess/Queries/AccountsQueries.cs
@@ -0,0 +1,56 @@
+﻿using Accounting.Application.Queries;
+using Accounting.Application.Results;
+using Accounting.Infrastructure.Persistance.MongoDataAccess.Entities;
+using System;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Accounting.Infrastructure.Exceptions;
+
+namespace Accounting.Infrastructure.Persistance.MongoDataAccess.Queries
+{
+    public class AccountsQueries : IAccountQueries
+    {
+        private readonly Context _context;
+        public AccountsQueries(Context context)
+        {
+            this._context = context;
+        }
+        public async Task<AccountResult> GetAccount(Guid accountId)
+        {
+            Account data = await _context
+                .Accounts
+                .Find(account => account.Id == accountId)
+                .SingleOrDefaultAsync();
+            if (data == null)
+                throw new AccountNotFoundException($"The account {accountId} does not exist or has been closed");
+            List<Credit> credits = await _context
+                .Credits
+                .Find(e => e.AccountId == accountId)
+                .ToListAsync();
+            List<Debit> debits = await _context
+                .Debits
+                .Find(e => e.AccountId == accountId)
+                .ToListAsync();
+            double creditSum = credits.Sum(c => c.Amount);
+            double debitSum = debits.Sum(d => d.Amount);
+            List<TransactionResult> transactionResults = new List<TransactionResult>();
+            foreach (Credit credit in credits)
+            {
+                TransactionResult transaction = new TransactionResult(credit.Description, credit.Amount, credit.TransactionDate);
+                transactionResults.Add(transaction);
+            }
+            foreach(Debit debit in debits)
+            {
+                TransactionResult transaction = new TransactionResult(debit.Description, debit.Amount, debit.TransactionDate);
+                transactionResults.Add(transaction);
+            }
+
+            List<TransactionResult> orderedTransactions = transactionResults.OrderBy(e => e.TransactionDate).ToList();
+            AccountResult accountResult = new AccountResult(data.Id, creditSum - debitSum, orderedTransactions);
+            return accountResult;
+        }
+    }
+}
---
+++ src/Accounting.Infrastructure/Persistance/MongoDataAccess/Queries/CustomerQueries.cs
@@ -0,0 +1,50 @@
+﻿using Accounting.Application.Queries;
+using Accounting.Application.Results;
+using Accounting.Infrastructure.Persistance.MongoDataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Accounting.Infrastructure.Exceptions;
+
+namespace Accounting.Infrastructure.Persistance.MongoDataAccess.Queries
+{
+    public class CustomerQueries : ICustomerQueries
+    {
+        private readonly Context _context;
+        private readonly IAccountQueries _accountQueries;
+
+        public CustomerQueries(Context context, IAccountQueries accountQueries)
+        {
+            _context = context;
+            _accountQueries = accountQueries;
+        }
+        public async Task<CustomerResult> GetCustomer(Guid customerId)
+        {
+            Customer customer = await _context.Customers
+              .Find(customer => customer.Id == customerId).SingleOrDefaultAsync();
+
+            if (customer == null)
+                throw new CustomerNotFoundException($"The {customerId} does not exist");
+            
+            List<Guid> accountIds = await _context
+                .Accounts
+                .Find(e => e.CustomerId == customerId)
+                .Project(p => p.Id)
+                .ToListAsync();
+
+            List<AccountResult> accountResults = new List<AccountResult>();
+            foreach(Guid accountId in accountIds)
+            {
+                 AccountResult accountResult = await _accountQueries.GetAccount(accountId);
+                accountResults.Add(accountResult);
+            }
+
+            CustomerResult customerResult = new CustomerResult(customer.Id, customer.Name, customer.Aadhar, accountResults);
+            return customerResult;
+         }
+
+
+    }
+}
