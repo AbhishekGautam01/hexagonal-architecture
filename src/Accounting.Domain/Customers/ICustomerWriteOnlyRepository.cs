65cb1ee8e8b7d22a97f2eb7083ff417c96ff7640
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 11:40:17 +0530

    Added Customer Repository Contract

---
+++ src/Accounting.Domain/Customers/ICustomerWriteOnlyRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Domain.Customers
+{
+    public interface ICustomerWriteOnlyRepository
+    {
+        Task Add(Customer customer);
+        Task Update(Customer customer);
+    }
+}
