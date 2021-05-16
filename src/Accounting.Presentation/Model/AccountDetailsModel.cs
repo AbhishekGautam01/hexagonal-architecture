75b5e8ef877fe3d86f10f3f9e662da68458005d5
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 20:34:23 +0530

    Added Models

---
+++ src/Accounting.Presentation/Model/AccountDetailsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accounting.Presentation.Model
+{
+    public sealed class AccountDetailsModel
+    {
+        public Guid AccountId { get; }
+        public double CurrentBalance { get; }
+        public List<TransactionModel> Transactions { get; }
+
+        public AccountDetailsModel(Guid accountId, double currentBalance, List<TransactionModel> transactions)
+        {
+            AccountId = accountId;
+            CurrentBalance = currentBalance;
+            Transactions = transactions;
+        }
+    }
+}
