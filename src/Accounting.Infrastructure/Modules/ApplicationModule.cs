bc0b5ffb0c67f5b85cc072ec8bd8ed11df6d4f85
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 19:11:46 +0530

    Added Application Module DI Config

---
+++ src/Accounting.Infrastructure/Modules/ApplicationModule.cs
@@ -0,0 +1,19 @@
+﻿namespace Accounting.Infrastructure.Modules
+{
+    using Autofac;
+    using Accounting.Application;
+    using Accounting.Application.Exceptions;
+
+    public class ApplicationModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //
+            // Register all Types in Acerola.Application
+            //
+            builder.RegisterAssemblyTypes(typeof(ApplicationException).Assembly)
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
