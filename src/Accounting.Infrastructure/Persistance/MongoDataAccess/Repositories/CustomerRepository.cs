22852020809cc8dd7453c641ecb8ab563a9d30c8
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 17:41:52 +0530

    [Co-Author]: aashiGupta01 Impl Customer Repository

---
+++ src/Accounting.Infrastructure/Persistance/MongoDataAccess/Repositories/CustomerRepository.cs
@@ -0,0 +1,68 @@
+﻿using Accounting.Domain.Customers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Accounting.Infrastructure.Persistance.MongoDataAccess.Repositories
+{
+    public class CustomerRepository : ICustomerReadOnlyRepository, ICustomerWriteOnlyRepository
+    {
+        private readonly Context _context;
+
+        public CustomerRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Customer customer)
+        {
+            Entities.Customer customerEntity = new Entities.Customer
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Aadhar = customer.Aadhar,
+            };
+            await _context.Customers.InsertOneAsync(customerEntity);
+        }
+
+        public async Task<Customer> Get(Guid id)
+        {
+            Entities.Customer customer = await _context.Customers.
+                  Find(customer => customer.Id == id)
+                  .SingleOrDefaultAsync();
+            
+            List<Guid> accountIds = await _context
+                .Accounts
+                .Find(account => account.CustomerId == id)
+                .Project(p => p.Id)
+                .ToListAsync();
+            
+            AccountCollection accountCollection = new AccountCollection();
+            foreach (var accountId in accountIds)
+            {
+                accountCollection.Add(accountId);
+            }
+
+            return Customer.Load(
+                customer.Id,
+                customer.Name,
+                customer.Aadhar,
+                accountCollection);
+        }
+
+
+        public async Task Update(Customer customer)
+        {
+            Entities.Customer customerEntity = new Entities.Customer
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Aadhar = customer.Aadhar,
+            };
+            var filter = Builders<Entities.Customer>.Filter.Eq(e => e.Id, customerEntity.Id);
+            await _context.Customers.ReplaceOneAsync(filter, customerEntity);
+        }
+    }
+}
