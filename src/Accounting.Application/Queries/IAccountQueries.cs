d99971d062982ae4d7bd4cfb12c05ad88eb3d8d7
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 12:12:12 +0530

    Added Account Query Contract

---
+++ src/Accounting.Application/Queries/IAccountQueries.cs
@@ -0,0 +1,13 @@
+﻿using Accounting.Application.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Application.Queries
+{
+    public interface IAccountQueries
+    {
+        Task<AccountResult> GetAccount(Guid accountId);
+    }
+}
---
+++ src/Accounting.Application/Results/TransactionResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting.Application.Results
+{
+    public sealed class TransactionResult
+    {
+        public string Description { get; }
+        public double Amount { get; }
+        public DateTime TransactionDate { get; }
+        public TransactionResult(
+            string description,
+            double amount, 
+            DateTime transactionDate)
+        {
+            Description = description;
+            Amount = amount;
+            TransactionDate = transactionDate;
+        }
+    }
+}
