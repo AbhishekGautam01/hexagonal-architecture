186b34eb75c61360edd9dd3c187a9cb8e3b42988
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 11:37:34 +0530

    Added Accounting Repository Contract

---
+++ src/Accounting.Domain/Accounts/IAccountWriteOnlyRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Domain.Accounts
+{
+    public interface IAccountWriteOnlyRepository
+    {
+        Task Add(Account account, Credit credit);
+        Task Update(Account account, Credit credit);
+        Task Update(Account account, Debit debit);
+        Task Delete(Account account);
+    }
+}
