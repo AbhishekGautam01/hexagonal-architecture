6f5950ba589dbe96d0a6d24c2cc8206e3205eec9
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 18:45:12 +0530

    EF impl

---
+++ src/Accounting.Infrastructure/Persistance/EntityFrameworkDataAccess/Entities/Debit.cs
@@ -0,0 +1,12 @@
+﻿namespace Accounting.Infrastructure.EntityFrameworkDataAccess.Entities
+{
+    using System;
+
+    public class Debit
+    {
+        public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        public double Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
