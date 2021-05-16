f218cf18990fc942ff3fa5566bbadfcf7ba62b42
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 17:49:00 +0530

    Mongo Data Access DI

---
+++ src/Accounting.Infrastructure/Persistance/MongoDataAccess/Module.cs
@@ -0,0 +1,28 @@
+﻿using Accounting.Infrastructure.Exceptions;
+using Autofac;
+
+namespace Accounting.Infrastructure.Persistance.MongoDataAccess
+{
+    public class Module: Autofac.Module
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Context>()
+                .As<Context>()
+                .WithParameter("connectionString", ConnectionString)
+                .WithParameter("databseName", DatabaseName)
+                .SingleInstance();
+
+            // Register all Types in MongoDataAccess
+            builder.RegisterAssemblyTypes(typeof(InfrastructureException).Assembly)
+                .Where(type => type.Namespace.Contains("MongoDataAccess"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+
+
+
+    }
+}
