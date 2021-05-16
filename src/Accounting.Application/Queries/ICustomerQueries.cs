8cc8c4e0819b23d1d90fa3b19269c733c2028769
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 12:19:26 +0530

    Customer Query Contract Impl

---
+++ src/Accounting.Application/Queries/ICustomerQueries.cs
@@ -0,0 +1,13 @@
+﻿using Accounting.Application.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Application.Queries
+{
+    public interface ICustomerQueries
+    {
+        Task<CustomerResult> GetCustomer(Guid customerId);
+    }
+}
---
+++ src/Accounting.Application/Results/CustomerResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting.Application.Results
+{
+    public sealed class CustomerResult
+    {
+        public Guid CustomerId { get; }
+        public string PersonNummer { get; }
+        public string Name { get; }
+        public IReadOnlyList<AccountResult> Accounts { get; }
+        public CustomerResult(
+            Guid customerId, 
+            string personNummer,
+            string name, 
+            List<AccountResult> accounts)
+        {
+            CustomerId = customerId;
+            PersonNummer = personNummer;
+            Name = name;
+            Accounts = accounts;
+        }
+    }
+}
