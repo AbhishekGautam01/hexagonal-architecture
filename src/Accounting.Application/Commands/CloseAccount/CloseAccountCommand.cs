345e07949300948934436284a3798cffc5f22123
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-05-16 14:53:28 +0530

    Close Account Command Impl

---
+++ src/Accounting.Application/Commands/CloseAccount/CloseAccountCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting.Application.Commands.CloseAccount
+{
+    public sealed class CloseAccountCommand : ICommand
+    {
+        private CloseAccountCommand() { }
+        public CloseAccountCommand(Guid accountId)
+        {
+            AccountId = accountId;
+        }
+        public Guid AccountId { get; private set; }
+    }
+}
---
+++ src/Accounting.Application/Commands/CloseAccount/CloseAccountCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Accounting.Application.Exceptions;
+using Accounting.Domain.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Application.Commands.CloseAccount
+{
+    public sealed class CloseAccountCommandHandler : ICommandHandler<CloseAccountCommand, Guid>
+    {
+        private readonly IAccountReadOnlyRepository _accountReadOnlyRepository;
+        private readonly IAccountWriteOnlyRepository _accountWriteOnlyRepository;
+        public CloseAccountCommandHandler(
+            IAccountReadOnlyRepository accountReadOnlyRepository,
+            IAccountWriteOnlyRepository accountWriteOnlyRepository)
+        {
+            _accountReadOnlyRepository = accountReadOnlyRepository;
+            _accountWriteOnlyRepository = accountWriteOnlyRepository;
+        }
+        public async Task<Guid> Execute(CloseAccountCommand command)
+        {
+            Account account = await _accountReadOnlyRepository.Get(command.AccountId);
+            if (account == null)
+                throw new AccountNotFoundException($"The account {command.AccountId} does not exist or it is already closed");
+            account.Close();
+            await _accountWriteOnlyRepository.Delete(account);
+            return account.Id;
+        }
+    }
+}
